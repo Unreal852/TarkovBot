c4aa01a9e1b16ebdd48d30de89ec8c793fff8cd8
Unreal852 <dodo-pl-08@hotmail.fr>
2022-04-20 03:32:53 +0200

    Ammo

---
+++ TarkovRatBot/Tarkov/AmmoInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TarkovRatBot.Tarkov;
+
+public class AmmoInfo
+{
+    [JsonPropertyName("ammoType")]     public string AmmoType     { get; set; }
+    [JsonPropertyName("caliber")]      public string Caliber      { get; set; }
+    [JsonPropertyName("maxStackSize")] public int?   MaxStackSize { get; set; }
+    [JsonPropertyName("damage")]       public int?   Damage       { get; set; }
+    [JsonPropertyName("armorDamage")]  public int?   ArmorDamage  { get; set; }
+
+    [JsonPropertyName("penetrationChance")]
+    public float? PenetrationChance { get; set; }
+
+    [JsonPropertyName("penetrationPower")] public int?   PenetrationPower { get; set; }
+    [JsonPropertyName("ricochetChance")]   public float? RicochetChance   { get; set; }
+
+    [JsonPropertyName("fragmentationChance")]
+    public float? FragmentationChance { get; set; }
+
+    [JsonPropertyName("initialSpeed")] public int? InitialSpeed { get; set; }
+
+    [JsonPropertyName("LightBleedModifier")]
+    public float? LightBleedModifier { get; set; }
+
+    [JsonPropertyName("heavyBleedModifier")]
+    public float? HeavyBleedModifier { get; set; }
+
+    [JsonPropertyName("tracer")] public bool     Tracer { get; set; }
+    [JsonPropertyName("item")]   public ItemInfo Item   { get; set; }
+}
